06a4c21a4615b818d8de3bef1bb315dd6cf3780c
Harry Barber <hlbarber93@gmail.com>
2019-07-19 22:38:08 +0700

    Improve Basic example

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -1,8 +1,8 @@
 use bitcoin_zmq::Subscriber;
-use futures::{Future, Stream};
+use futures::{Future, Stream, lazy};
 
 fn main() {
-    let subscriber = Subscriber::new("tcp://35.222.194.216:28332", 1024);
+    let subscriber = Subscriber::new("tcp://127.0.0.1:28332", 1024);
     let print_hashes = subscriber.subscribe_raw_tx().for_each(|hash| {
         println!("raw tx: {:?}", hash);
         Ok(())
@@ -12,7 +12,9 @@ fn main() {
         .connect()
         .map_err(|err| println!("err = {:?}", err));
 
-    let runner = connect.and_then(|_| print_hashes.map_err(|err| println!("err = {:?}", err)));
-
-    tokio::run(runner)
+    tokio::run(lazy(|| {
+        tokio::spawn(connect);
+        tokio::spawn(print_hashes);
+        Ok(())
+    }) )
 }
